b3a3db4976371fc6a0a04bda2b64b7a2c1cdeb65
Kuter <BKUTER@mgh.harvard.edu>
2018-03-19 13:43:15 -0400

    update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,13 +6,17 @@ import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
+import { MessageService } from './message.service';
+
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
 }
